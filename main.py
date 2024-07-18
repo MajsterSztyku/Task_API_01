0239a59f25a9fd217f0fc03ed14d1d01e9ee4796
MajsterSztyku <michant100@yahoo.co.uk>
2024-07-18 13:55:06 +0200

    basically done

--- main.py
+++ main.py
@@ -8,13 +8,25 @@ app = FastAPI()
 
 @app.post("/csv_to_json")
 async def csv_to_json(file: UploadFile = File(...)):
+    if file.content_type != "text/csv":
+        raise HTTPException(status_code=400, detail="wrong file")
     # with open("test.csv",mode = "+r",newline="")as plik:
     #     read = csv.reader(plik)
     # <--- nie działa cały komentarz. Zrozumieć czemu
     content = await file.read()
     file_content = io.StringIO(content.decode("utf-8"))
-    reader = csv.DictReader(file_content)
-    rows= []
+    reader = csv.DictReader(file_content) # dictreader pozwala odczytać plik csv w postaci słownika
+    json_data = {}
     for row in reader:
-        rows.append(row)
-    return rows
+        for key, value in row.items():
+            if key in json_data:
+                json_data[key].append(value)
+            else:
+                json_data[key] = [value]
+    #json_output = json.dumps(json_data,ensure_ascii=False)
+    # użycie tego zapisu sprawi że dane JSON będą kodowane jako ciąg znaków a nie jako obiekt JSON.
+    # Zamiast używać json.dumps można wykożystać JSONResponse który automatycznie 
+    # przekształci dane w odpowiedni obiekt JSON 
+
+
+    return JSONResponse(content=json_data) 
